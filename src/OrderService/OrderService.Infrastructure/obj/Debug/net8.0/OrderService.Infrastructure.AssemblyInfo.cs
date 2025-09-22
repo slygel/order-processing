47ffc8b1e61804305b4caad4fa794fb690cc926c
Tue Nguyen Tai <tue.nguyentai@nashtechglobal.com>
2025-09-22 20:57:18 +0700

    update k8s

--- src/OrderService/OrderService.Infrastructure/obj/Debug/net8.0/OrderService.Infrastructure.AssemblyInfo.cs
+++ src/OrderService/OrderService.Infrastructure/obj/Debug/net8.0/OrderService.Infrastructure.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("OrderService.Infrastructure")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ea7893aaebf0b71155ae62b4d6752915bea2f314")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d1a762d2f37cac21503eafbdc13e61836cc68769")]
 [assembly: System.Reflection.AssemblyProductAttribute("OrderService.Infrastructure")]
 [assembly: System.Reflection.AssemblyTitleAttribute("OrderService.Infrastructure")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
